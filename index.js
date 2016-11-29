8de1bef67a6fe95aeaa37c33772cdfe239586891
AlexLee <alexlee7171@gmail.com>
2016-11-29 13:12:52 +0800

    add ejs render engine

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var express = require("express");
-var expressGit = require("express-git");
-var app = express();
+const express = require("express");
+const expressGit = require("express-git");
+const ejs = require('ejs');
+const app = express();
+
+
+app.set('views', __dirname + '/views');
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+	res.render('index');
+})
+
 app.use("/git", expressGit.serve("git-repo", {
 	    auto_init: true,
 	    serve_static: true,
